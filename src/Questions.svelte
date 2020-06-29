af43ee4b562c4503f7488bc4dcce2166075f8cff
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2020-06-28 23:06:47 -0500

    Algo super machetudo

--- src/Questions.svelte
+++ src/Questions.svelte
@@ -1,10 +1,41 @@
 <script>
-    import VoteBtn from './VoteBtn.svelte'
-
 	const fetchQuestions = (async () => {
         const response = await fetch('http://localhost:1337/questions')
         return response.json()
     })()
+
+    const handleInput = (event) => {
+        console.time("Voto")
+        let idAnswer = event.target.getAttribute('data-answer-id')
+        vote(idAnswer)
+        console.timeEnd("Voto")
+    }
+
+    const vote = async (idAnswer) => {
+
+        let votesCurrent = 0;
+
+        const fetchAnswer = await fetch(`http://localhost:1337/answers/${idAnswer}`)
+                                .then(res => res.json())
+                                .then((data) => {
+                                    votesCurrent = data.votes
+                                })
+
+        await fetch(`http://localhost:1337/answers/${idAnswer}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                votes: votesCurrent + 1
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            const span = document.getElementById(idAnswer)
+            span.innerHTML = data.votes 
+        })
+    }
 </script>
 
 <style>
@@ -15,6 +46,15 @@
     }
     span {
         color:#f44336;
+        text-decoration-line: underline;
+    }
+    button {
+        background-color: #4caf50;
+        color: white;
+        border-radius: 6px;
+    }
+    button:active {
+        background-color: #1b5e20;
     }
 </style>
 
@@ -31,7 +71,12 @@
          <ul>
         {#each question.answers as answer}
             <li>
-                <span>({answer.votes})</span> {answer.content} <VoteBtn idAnswer={answer._id} />
+                <div>
+                    <span id={answer._id}>{answer.votes}</span>
+                    {answer.content}
+                    <button data-answer-id={answer._id} on:click={handleInput}>+1</button>
+                </div>
+
             </li>    
         {/each}
         </ul>
