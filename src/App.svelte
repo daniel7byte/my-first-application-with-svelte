bccc9ad39b1b3eee0a6ff7c05f523a74a0a3fdb0
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2020-07-03 11:35:07 -0500

    Funciona mas o menos. Con F5 peta!

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,27 @@
 <script>
-	import Questions from './Questions.svelte'
-	import SearchMovies  from './SearchMovies.svelte'
+	import { Router, Link, Route } from "svelte-routing";
+	import Questions from "./routes/Questions.svelte";
+	import SearchMovies from "./routes/SearchMovies.svelte";
+  
+	export let url = "";
 </script>
 
-<Questions />
-<SearchMovies />
+<style>
+	nav {
+		color: aqua;
+		background-color: blueviolet;
+	}
+</style>
+
+<Router url="{url}">
+	<nav>
+		<a href="/replace" replace>Replace this URL</a>
+		<a href="/native" noroute>Use the native action</a>
+		<Link to="/">Questions</Link>
+		<Link to="SearchMovies">SearchMovies</Link>
+	</nav>
+	<div>
+		<Route path="/" component={Questions} />
+		<Route path="SearchMovies" component={SearchMovies} />
+	</div>
+</Router>
