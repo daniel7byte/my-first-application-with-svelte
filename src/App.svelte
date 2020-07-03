801ee7d9ea2bf9507ca3285bfa872200d40fdbf6
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2020-07-03 11:58:37 -0500

    404

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
 	import { Router, Link, Route } from "svelte-routing";
 	import Questions from "./routes/Questions.svelte";
 	import SearchMovies from "./routes/SearchMovies.svelte";
+	import NotFound from "./routes/NotFound.svelte";
   
 	export let url = "";
 </script>
@@ -15,13 +16,12 @@
 
 <Router url="{url}">
 	<nav>
-		<a href="/replace" replace>Replace this URL</a>
-		<a href="/native" noroute>Use the native action</a>
 		<Link to="/">Questions</Link>
 		<Link to="SearchMovies">SearchMovies</Link>
 	</nav>
 	<div>
 		<Route path="/" component={Questions} />
 		<Route path="SearchMovies" component={SearchMovies} />
+		<Route component="{NotFound}" ><NotFound url={url} /></Route>
 	</div>
 </Router>
---
+++ src/routes/NotFound.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let url;
+</script>
+
+<h1>Pagina no encontrada 404</h1>
+<p>URL: {url}</p>
